93ec168cb202b808aa9544d63f019d2c276e5449
TurboGooseEdu <st076013@student.spbu.ru>
2023-11-28 12:35:58 +0300

    Fix shell errors with respect to ShellCheck

--- 02.Desktop_Search/import_umd.sh
+++ 02.Desktop_Search/import_umd.sh
@@ -4,13 +4,13 @@ SRC="https://spbu.ru/sveden/education"
 DST="./result"
 PRB=false
 
-OPTS=$(getopt -o s:d:p --long source:,destination:,probe -- $@)
+OPTS=$(getopt -o s:d:p --long source:,destination:,probe -- "$@")
 if [ $? -ne 0 ]; then
     echo "Wrong input"
     exit 1
 fi
 
-eval set -- $OPTS
+eval set -- "$OPTS"
 
 while true; do
     case $1 in
@@ -37,29 +37,29 @@ while true; do
 done
 
 if [ -z "$SRC" ] || [ -z "$DST" ]; then
-    exit -1
+    exit 1
 fi
 
 
-mkdir -p $DST
+mkdir -p "$DST"
 
 
 dl_spbu_s_e () {
-    curl $SRC -s --compressed -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8" -H "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3" -H "Accept-Encoding: gzip, deflate, br" -H "DNT: 1" -H "Connection: keep-alive"
+    curl "$SRC" -s --compressed -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8" -H "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3" -H "Accept-Encoding: gzip, deflate, br" -H "DNT: 1" -H "Connection: keep-alive"
 }
 
 dl_spbu_oop () {
     local result=$(dl_spbu_s_e grep -o -E "'https://nc\.spbu\.ru/.+?'" | sed "s/'//g" | grep -oP 'https://nc\.spbu\.ru/s/[^\s<>"'\'']+' | sort | uniq )
     if [ $PRB == true ]; then
         echo "probing"
-        return $result | sort -R | head -n 3
+        return "$result" | sort -R | head -n 3
     else
-        return $result
+        return "$result"
     fi
 }
 
 function download {
-    if ! wget -P $DST $1/download; then
+    if ! wget -P "$DST" "$1/download"; then
         >&2 echo "Cannot download $1"
         return 15
     fi
@@ -67,16 +67,16 @@ function download {
 
 
 function unarchive {
-    for file in $1/*; do
-        if [ -f $file ]; then
-            unar -f $file -o $1
+    for file in "$1"/*; do
+        if [ -f "$file" ]; then
+            unar -f "$file" -o "$1"
         fi
     done
 }
 
 
 for url in $(dl_spbu_oop); do
-    download ${url}
+    download "${url}"
 done
 
-unarchive $DST
+unarchive "$DST"
