c996b8106affbe690d998c869e323a39c4f449fd
TurboGooseEdu <st076013@student.spbu.ru>
2023-11-28 17:47:18 +0300

    Fix downloading and implement indexing using recoll

--- 02.Desktop_Search/import_umd.sh
+++ 02.Desktop_Search/import_umd.sh
@@ -48,30 +48,45 @@ dl_spbu_s_e () {
     curl "$SRC" -s --compressed -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8" -H "Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3" -H "Accept-Encoding: gzip, deflate, br" -H "DNT: 1" -H "Connection: keep-alive"
 }
 
-dl_spbu_oop () {
-    local result=$(dl_spbu_s_e grep -o -E "'https://nc\.spbu\.ru/.+?'" | sed "s/'//g" | grep -oP 'https://nc\.spbu\.ru/s/[^\s<>"'\'']+' | sort | uniq )
-    if [ $PRB == true ]; then
-        echo "probing"
-        return "$result" | sort -R | head -n 3
+dl_spbu_oop() {
+    local result
+    result=$(dl_spbu_s_e grep -o -E "'https://nc\.spbu\.ru/.+?'" | sed "s/'//g" | \
+             grep -oP 'https://nc\.spbu\.ru/s/[^\s<>"'\'']+' | sort | uniq )
+    if [ "$PRB" = true ]; then
+        echo "$result" | sort -R | head -n 3
     else
-        return "$result"
+        echo "$result"
     fi
+    
 }
 
-function download {
+
+download() {
     if ! wget -P "$DST" "$1/download"; then
         >&2 echo "Cannot download $1"
         return 15
     fi
 }
 
-
-function unarchive {
+unarchive() {
     for file in "$1"/*; do
         if [ -f "$file" ]; then
-            unar -f "$file" -o "$1"
+            unar -q -f "$file" -o "$1"
+            rm "$file"
         fi
     done
+}
+
+index() {
+    echo "Indexing..."
+    recollindex -r "$1"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        >&2 echo "Failed to update index."
+        return $status
+    else
+        echo "Index updated."
+    fi
 }
 
 
@@ -80,3 +95,4 @@ for url in $(dl_spbu_oop); do
 done
 
 unarchive "$DST"
+index "$DST"
